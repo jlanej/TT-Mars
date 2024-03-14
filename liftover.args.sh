dd16c84d81792ceb6a01564fbf7947c3d68a6ba6
jlanej <jlanej@gmail.com>
2024-03-14 16:01:00 -0500

    refactor scriptDir

--- liftover.args.sh
+++ liftover.args.sh
@@ -5,7 +5,7 @@ output_dir=$2
 if_hg38=$3
 h1=$4
 h2=$5
-script_dir=$6
+scriptDir=$6
 
 
 #1. Align assembly to reference
@@ -34,8 +34,8 @@ fi
 #2. Trim overlapping contigs
 
 #trim overlapping contigs
-python $script_dir/trim_overlapping_contigs.py assem1_sort.bam $output_dir $if_hg38
-python $script_dir/trim_overlapping_contigs.py assem2_sort.bam $output_dir $if_hg38
+python "$scriptDir"/trim_overlapping_contigs.py assem1_sort.bam $output_dir $if_hg38
+python "$scriptDir"/trim_overlapping_contigs.py assem2_sort.bam $output_dir $if_hg38
 
 #sort trimmed bam file
 samtools sort $output_dir/assem1_sort_nool.bam -o $output_dir/assem1_nool_sort.bam
@@ -54,13 +54,13 @@ samtools view -h $output_dir/assem1_nool_sort.bam | samtools sort -O sam -o $out
 samtools view -h $output_dir/assem2_nool_sort.bam | samtools sort -O sam -o $output_dir/assem2_nool_sort.sam
 
 #liftover using samLiftover (https://github.com/mchaisso/mcutils): ref to asm lo
-python $script_dir/lo_assem_to_ref.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam
+python "$scriptDir"/lo_assem_to_ref.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam
 
 samLiftover $output_dir/assem1_nool_sort.sam $output_dir/lo_pos_assem1.bed $output_dir/lo_pos_assem1_result.bed --dir 1
 samLiftover $output_dir/assem2_nool_sort.sam $output_dir/lo_pos_assem2.bed $output_dir/lo_pos_assem2_result.bed --dir 1
 
 #liftover using samLiftover (https://github.com/mchaisso/mcutils): asm to ref lo
-python $script_dir/lo_assem_to_ref_0.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam
+python "$scriptDir"/lo_assem_to_ref_0.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam
 
 samLiftover $output_dir/assem1_nool_sort.sam $output_dir/lo_pos_assem1_0.bed $output_dir/lo_pos_assem1_0_result.bed --dir 0
 samLiftover $output_dir/assem2_nool_sort.sam $output_dir/lo_pos_assem2_0.bed $output_dir/lo_pos_assem2_0_result.bed --dir 0
@@ -69,13 +69,13 @@ samLiftover $output_dir/assem2_nool_sort.sam $output_dir/lo_pos_assem2_0.bed $ou
 #######################################################
 #4. Compress liftover files
 
-python $script_dir/compress_liftover.py $output_dir lo_pos_assem1_result.bed lo_pos_assem1_result_compressed.bed
-python $script_dir/compress_liftover.py $output_dir lo_pos_assem2_result.bed lo_pos_assem2_result_compressed.bed
-python $script_dir/compress_liftover.py $output_dir lo_pos_assem1_0_result.bed lo_pos_assem1_0_result_compressed.bed
-python $script_dir/compress_liftover.py $output_dir lo_pos_assem2_0_result.bed lo_pos_assem2_0_result_compressed.bed
+python "$scriptDir"/compress_liftover.py $output_dir lo_pos_assem1_result.bed lo_pos_assem1_result_compressed.bed
+python "$scriptDir"/compress_liftover.py $output_dir lo_pos_assem2_result.bed lo_pos_assem2_result_compressed.bed
+python "$scriptDir"/compress_liftover.py $output_dir lo_pos_assem1_0_result.bed lo_pos_assem1_0_result_compressed.bed
+python "$scriptDir"/compress_liftover.py $output_dir lo_pos_assem2_0_result.bed lo_pos_assem2_0_result_compressed.bed
 
 #######################################################
 #######################################################
 #5. Get non-covered regions
 
-python $script_dir/get_conf_int.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam $if_hg38
+python "$scriptDir"/get_conf_int.py $output_dir $output_dir/assem1_nool_sort.bam $output_dir/assem2_nool_sort.bam $if_hg38
